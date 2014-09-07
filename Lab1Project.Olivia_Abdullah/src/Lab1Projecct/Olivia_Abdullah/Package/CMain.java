d650ac2ab833584368b98b8f6c90f5f02c571efc
LENOVO <LENOVO@128.4.203.193>
2014-09-07 22:06:08 +0400

    lab1 almost done

--- Lab1Project.Olivia_Abdullah/src/Lab1Projecct/Olivia_Abdullah/Package/CMain.java
+++ Lab1Project.Olivia_Abdullah/src/Lab1Projecct/Olivia_Abdullah/Package/CMain.java
@@ -1,12 +1,17 @@
 package Lab1Projecct.Olivia_Abdullah.Package;
 
-public class CMain {
+
+public class CMain{
 
 	public static void main(String[] args) {
 		// Test
 		// Second Test for Repository Clone
 		// another test comment
-
+	double c = 2.5;
+	double d = 4.5;
+	
+	System.out.println(c+d);
+	
 	}
 
 }
