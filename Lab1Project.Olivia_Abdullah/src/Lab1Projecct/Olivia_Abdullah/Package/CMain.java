3e7e1dd4cf15c5cc64c57b2ab1c721d885a75e48
LENOVO <LENOVO@128.4.203.193>
2014-09-07 22:10:13 +0400

    lab1 cisc181

--- Lab1Project.Olivia_Abdullah/src/Lab1Projecct/Olivia_Abdullah/Package/CMain.java
+++ Lab1Project.Olivia_Abdullah/src/Lab1Projecct/Olivia_Abdullah/Package/CMain.java
@@ -1,5 +1,7 @@
 package Lab1Projecct.Olivia_Abdullah.Package;
 
+import java.util.Scanner;
+
 
 public class CMain{
 
@@ -7,11 +9,43 @@ public class CMain{
 		// Test
 		// Second Test for Repository Clone
 		// another test comment
-	double c = 2.5;
-	double d = 4.5;
+		Scanner x = new Scanner(System.in);
+		System.out.println("How many years you intednt to work?");
+		double years_of_work = x.nextInt();	
+		
+		System.out.println("Expected Average return on investment (0%-20%): ");
+		double ave_exp_return = x.nextInt();	
+		
+		
+		System.out.println("How long you intend to draw? ");
+		double years_of_draw = x.nextInt();	
+		
+		
+		System.out.println("Expected Annual return when in payback mode (0%-3%): ");
+		double payback_return = x.nextInt();
+		
+		
+		System.out.println("Required Income: ");
+		double income = x.nextInt();
+		
+		
+		System.out.println("Expected SSI income: ");
+		double SSI = x.nextInt();	
 	
-	System.out.println(c+d);
+		
+		
+		double return_average = ((income-SSI)*((1- Math.pow(1+((payback_return/100)/12), -(years_of_draw*12)))/((payback_return/100)/12)));
+		
+		String ave = String.format("%.2f", return_average);
+		
+		double monthly_save= return_average/ (((Math.pow(1+(((ave_exp_return/100)/12)), (years_of_work*12))-1)/((ave_exp_return/100)/12))); 
 	
-	}
+		
+		String mothlysaving = String.format("%.2f", monthly_save);
 
+		System.out.println("You will have to save: " + mothlysaving + " Dollars each month \nThe amount need to be saved is " + ave + " Dollars");
+	}
 }
+
+
+
